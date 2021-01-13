b3eee6ff992c2c2352cebdc57df9404585f1b2a0
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-01-13 11:01:19 -0300

    add some informations about numbers

---
+++ fundamentals/numbers.js
@@ -0,0 +1,33 @@
+const price = 50.0
+const price2 = Number('2.1')
+const price3 = Number('3.0')
+
+console.log(price);
+console.log(price2);
+console.log(price3);
+
+console.log(Number.isInteger(price));
+console.log(Number.isInteger(price2));
+console.log(Number.isInteger(price3));
+
+const n1 = 9.89
+const n2 = 7.75
+
+const total = n1 * 2 + n2 * 3
+const mean = total / 5
+
+console.log(mean);
+console.log(mean.toFixed(2));
+console.log(mean.toString());
+console.log(mean.toString(2)); // show mean as binary
+console.log(mean);
+
+console.log(10/0) //infinity
+console.log('10' / 2); // 5
+console.log('10.0' / 2); // 5
+console.log('10,0' / 2); // NaN
+console.log('show' * 2); // NaN
+console.log(0.1 + 0.7); //0.79999999
+console.log(9.5.toString());
+// console.log(10.toString()) SyntaxError: Invalid or unexpected token
+console.log((10).toString());
