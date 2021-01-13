a7136c304fd342e352f9cd100be520e31026f4d7
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-01-13 10:39:34 -0300

    Starting project
    This commit create fundamentals folder and start the studies about js with variables content

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Code from my studies about JS
 
 Since I have some dificulties using JavaScript, I've made this repo to learn more about JS
+
+for learning purposes, a lot of times I'll define variables with short names like a or b. This aspect will not happen in the construction of projects.
---
+++ fundamentals/variables.js
@@ -0,0 +1,21 @@
+var a = 3
+console.log(a)
+
+let b = 5
+console.log(b)
+
+var a = 5
+// let b = 10 -> This code will thow a syntax error
+b = 10
+console.log(a + b)
+
+const c = 100
+console.log(c);
+
+let some_word = 'fine'
+console.log(some_word);
+console.log(typeof some_word);
+
+some_word = 3.15
+console.log(some_word);
+console.log(typeof some_word);
